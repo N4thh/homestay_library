c7be674a9917d7fccf16d3077da200ddb742f84f
N4thh <nhatahh2003@gmail.com>
2025-06-26 16:37:58 +0900

    Revert "Update index.py"
    This reverts commit 55753b39e59c357d24d3b02c30967a418464d3bc.

--- api/index.py
+++ api/index.py
@@ -1,17 +1,7 @@
-try:
-    from pathlib import Path
-    import sys
-    sys.path.append(str(Path(__file__).resolve().parent.parent))
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-    from app import app
-except Exception as e:
-    import sys
-    import traceback
-    print('--- ERROR DURING APP IMPORT ---', file=sys.stderr)
-    print(str(e), file=sys.stderr)
-    traceback.print_exc(file=sys.stderr)
-    app = None
-    # Optionally, raise to force Vercel to show error
-    raise
+from app import app
 
 # Vercel sẽ tự động nhận biến app này 
